b0504c7df3234ea26736795677854509c8b0380c
cjmcewan <callum.mcewan@googlemail.com>
2020-05-22 13:36:42 +0100

    Create dsvmauthsetup.sh

---
+++ dsvmauthsetup.sh
@@ -0,0 +1,6 @@
+username="$1"
+passwordsecretname="$2"
+keyvaulturi="$3"
+functioncode=$(</home/dsvmadmin/functioncode.txt)
+password="$(curl "$keyvaulturi/api/Function1?Secret=$passwordsecretname&code=$functioncode\" 2>/dev/null | jq -r '.Value')"
+(echo '$password'; echo '$password') | sudo passwd $username
