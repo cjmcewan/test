40e349a02cee80c4e4d975a288ddbe39c985c16d
cjmcewan <callum.mcewan@googlemail.com>
2020-05-22 11:02:00 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,2 @@
 functioncode=$1
-username=$2
-$adminuser=$3
-sudo adduser $username --disabled-password --gecos GECOS
-sudo mkdir /home/$username/.ssh
-echo $functioncode >> /home/$adminuser/functioncode.txt
+echo $functioncode >> /home/dsvmadmin/functioncode.txt
