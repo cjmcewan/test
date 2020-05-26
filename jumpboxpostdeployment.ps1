76b1437d3879bd7ac2344be442ce6896c12412b2
cjmcewan <callum.mcewan@googlemail.com>
2020-05-26 16:02:33 +0100

    Update jumpboxpostdeployment.ps1

--- jumpboxpostdeployment.ps1
+++ jumpboxpostdeployment.ps1
@@ -1,4 +1,4 @@
-﻿Set-ExecutionPolicy RemoteSigned -force
+Set-ExecutionPolicy RemoteSigned -force
 
 $Username=$args[0]
 $Password=$args[1]
@@ -6,7 +6,58 @@ $Fullname=$args[2]
 $Description=$args[3]
 $UserGroup=$args[4]
 
+$methodName = 'UserEnvCP'
+$script:nativeMethods = @();
+
 $SecurePassword=ConvertTo-SecureString $Password –asplaintext –force 
+
+#function to register a native method
+function Register-NativeMethod
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([int])]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [string]$dll,
+ 
+        # Param2 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=1)]
+        [string]
+        $methodSignature
+    )
+ 
+    $script:nativeMethods += [PSCustomObject]@{ Dll = $dll; Signature = $methodSignature; }
+}
+
+#function to add native method
+function Add-NativeMethods
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([int])]
+    Param($typeName = 'NativeMethods')
+ 
+    $nativeMethodsCode = $script:nativeMethods | ForEach-Object { "
+        [DllImport(`"$($_.Dll)`")]
+        public static extern $($_.Signature);
+    " }
+ 
+    Add-Type @"
+        using System;
+        using System.Text;
+        using System.Runtime.InteropServices;
+        public static class $typeName {
+            $nativeMethodsCode
+        }
+"@
+}
 
 # Get local accounts module
 Get-Command -Module  Microsoft.PowerShell.LocalAccounts
@@ -18,10 +69,33 @@ Set-LocalUser -Name $Username -Fullname $Fullname
 #Add user to suitable group
 Add-LocalGroupMember -Group $UserGroup -Member $Username
 
+Register-NativeMethod "userenv.dll" "int CreateProfile([MarshalAs(UnmanagedType.LPWStr)] string pszUserSid,`
+  [MarshalAs(UnmanagedType.LPWStr)] string pszUserName,`
+  [Out][MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszProfilePath, uint cchProfilePath)";
+
+Add-NativeMethods -typeName $MethodName;
+
+$localUser = New-Object System.Security.Principal.NTAccount("$UserName");
+$userSID = $localUser.Translate([System.Security.Principal.SecurityIdentifier]);
+$sb = new-object System.Text.StringBuilder(260);
+$pathLen = $sb.Capacity;
+
+Write-Verbose "Creating user profile for $Username";
+try
+{
+    [UserEnvCP]::CreateProfile($userSID.Value, $Username, $sb, $pathLen) | Out-Null;
+}
+catch
+{
+    Write-Error $_.Exception.Message;
+    break;
+}
+
+
 # Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Install Software
 choco install visualstudiocode -y
-choco install X2Go -y
+choco install X2Go -y 
 choco install Putty -y
